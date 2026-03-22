953c4274a7131382bb1f14a738e5f03b11b53bbf
Nhan <23130217@st.hcmuaf.edu.vn>
2026-03-22 23:48:13 +0700

    update booking and create info-booking

---
+++ src/main/webapp/assets/js/info-booking.js
@@ -0,0 +1,7 @@
+document.querySelector('.btn-next').addEventListener('click', function() {
+    alert('Chuyển sang bước thanh toán');
+});
+
+document.querySelector('.btn-back').addEventListener('click', function() {
+    window.history.back();
+});
