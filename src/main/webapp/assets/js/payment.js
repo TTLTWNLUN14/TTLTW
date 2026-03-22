c33d0cd8bdc86e37f42e12dd609cb8ba7b3d1160
NgTrXuanDao <23130044@st.hcmuaf.edu.vn>
2026-03-22 23:27:28 +0700

    tạo giao diện trang thanh toán - thêm phần mã giảm giá

--- src/main/webapp/assets/js/payment.js
+++ src/main/webapp/assets/js/payment.js
@@ -1,10 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const paymentMethods = document.querySelectorAll('.payment-method');
-
-    paymentMethods.forEach(method => {
-        method.addEventListener('click', function() {
-            paymentMethods.forEach(m => m.classList.remove('active'));
+    const paymentCards = document.querySelectorAll('.payment-card');
+    paymentCards.forEach(card => {
+        card.addEventListener('click', function() {
+            paymentCards.forEach(m => m.classList.remove('active'));
             this.classList.add('active');
+        });
+    });
+    const couponItems = document.querySelectorAll('.coupon-item');
+    const discountInput = document.getElementById('discount-code');
+
+    couponItems.forEach(item => {
+        item.addEventListener('click', function() {
+            couponItems.forEach(c => c.classList.remove('active'));
+            this.classList.add('active');
+            const codeName = this.querySelector('strong').innerText;
+            if(discountInput) discountInput.value = codeName;
         });
     });
 });
