94c1e745ffd0d4ac3233d7296124c8cd3765b846
Huy <23130136@st.hcmuaf.edu.vn>
2026-03-27 18:47:19 +0700

    thiet lap account customer

--- src/main/webapp/assets/js/car-detail.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const thumbs = document.querySelectorAll('.cd-thumb');
-    thumbs.forEach(thumb => {
-        thumb.addEventListener('click', function() {
-            thumbs.forEach(t => t.classList.remove('active'));
-            this.classList.add('active');
-        });
-    });
-});
